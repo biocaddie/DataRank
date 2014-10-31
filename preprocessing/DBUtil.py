9b213be9e8cdefd7df58b670b275c765efcad4f3
arya <airanmehr@gmail.com>
2014-10-30 18:20:14 -0700

    Creating Raw Corpus

---
+++ preprocessing/DBUtil.py
@@ -0,0 +1,51 @@
+import sqlite3;
+
+class dbConnector():
+  def __init__(self, db_name):
+    self.src = sqlite3.connect(db_name);
+    self.srccur = self.src.cursor();
+    self.srccur.execute('SELECT id,article_meta FROM test_parser');
+    self.dst = sqlite3.connect('/'.join((db_name.split('/')[:-1]))+'/abstracts.db');
+    self.dstcur = self.dst.cursor();
+    self.dstcur.execute('drop table if exists abs_raw;');
+    self.dstcur.execute('drop table if exists dt_raw;');
+    self.dstcur.execute('drop table if exists td_raw;');
+    self.dstcur.execute('drop table if exists dic_raw;');
+    
+    self.dstcur.execute("create table abs_raw(id int primary key, abs text);");
+    self.dstcur.execute('create table dic_raw(id int primary key, term text);');
+    self.dstcur.execute('create table td_raw(id int primary key, docs text);');
+    self.dstcur.execute('create table dt_raw(id int primary key, terms text);');
+
+  def __enter__(self):
+    return self;
+
+  def __exit__(self, type, value, traceback):
+    self.src.close();
+
+  def getROW(self):
+      return self.srccur.fetchone()
+  
+      
+  def insertDoc(self,docID,abs, terms, table="raw"):
+    self.dstcur.execute('INSERT INTO abs_'+table+'(id, abs) VALUES (?, ?)', (docID,abs));
+    self.dstcur.execute('INSERT INTO dt_'+table+'(id, terms) VALUES (?, ?)', (docID,str(terms)));
+    print abs
+    print terms
+    for termID,num in terms.items():
+        self.dstcur.execute("select docs from td_"+table+" where id=?", (termID,))
+        docsOfTerm= self.dstcur.fetchone()
+        if docsOfTerm is None: # new term
+            rec =str({docID:num})
+            self.dstcur.execute('INSERT INTO td_'+table+'(id, docs) VALUES (?, ?)', (termID,rec));
+        else:
+            docsOfTerm=eval(docsOfTerm[0])
+            docsOfTerm[docID]=num
+            rec=str(docsOfTerm)
+            self.dstcur.execute("UPDATE td_"+table+" SET docs = ? WHERE id= ? """,(rec,termID))
+    self.dst.commit();
+    
+  def insertDic(self,dic, table="raw"):
+      for i in range(len(dic)):
+          self.dstcur.execute('INSERT INTO dic_'+table+'(id, term) VALUES (?, ?)', (i,dic[i]));
+      self.dst.commit();
---
+++ preprocessing/run.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import sys;
+import re;
+from preprocessing import DBUtil as lite
+from preprocessing import ExtractAbstract as ext
+
+def parse(doc):
+    doc=doc[doc.find('<abstract>'): doc.find('</abstract>')]
+    doc= re.sub('<[^>]*>', '', doc[doc.find('<abstract>'): doc.find('</abstract>')])
+#     doc= re.sub('[\n\t&#]', '', doc)
+    doc= re.sub('[\n\t]', '', doc)
+    doc= re.sub('\s+', ' ', doc).encode('ascii', errors='backslashreplace').lower()
+    return doc
+
+def insertDic(dic,doc):
+    words= doc.split()
+    terms={}
+    for w in words:
+        w=w.lower()
+        try:
+            idx= dic.index(w)
+        except ValueError:
+            idx=len(dic)
+            dic.append(w.lower())
+        if idx in terms:
+            terms[idx] += 1
+        else:
+            terms[idx] = 1
+    return dic, terms
+
+def main(argv):
+    terms=[]
+    dic=[]
+    j=0
+    path='/home/arya/server/public/hctest.db'
+    path='/home/public/hctest.db'
+    with lite.dbConnector(path) as db_conn:        
+        while 1:
+            j+=1
+            rec=db_conn.getROW()
+            if rec is None:
+                break
+            id,doc = rec[0], parse(rec[1])
+            dic, terms= insertDic(dic, doc)
+            if not id%100:
+                print id
+            db_conn.insertDoc(id, doc ,terms)
+#             if j>1:
+#                 break
+        db_conn.insertDic(dic)
+    print 'Done'
+if __name__ == '__main__':
+    main(sys.argv);
