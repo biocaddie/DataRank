645b9ddf019da008c0129711fc63ebe0dafde854
arya <airanmehr@gmail.com>
2014-10-31 18:47:48 -0700

    Cleaning: removing stopwords and  stemming

--- preprocessing/DBUtil.py
+++ preprocessing/DBUtil.py
@@ -1,26 +1,23 @@
 import sqlite3;
 
 class dbConnector():
-  def __init__(self, db_name, dbver="raw"):
-    self.dbver=dbver;
-    self.src = sqlite3.connect(db_name);
+  def __init__(self, param):
+    self.table_name=param['table_name'];
+    self.src = sqlite3.connect(param['src']);
     self.srccur = self.src.cursor();
-    if dbver=="raw":
-        self.srccur.execute('SELECT id,article_meta FROM test_parser');
-    else:
-        self.srccur.execute('SELECT id,abs FROM abs_raw');
-    self.dst = sqlite3.connect('/'.join((db_name.split('/')[:-1]))+'/abstracts_clean.db');
+    self.srccur.execute('SELECT id,article_meta FROM test_parser');
+    self.dst = sqlite3.connect(param['dst']);
     self.dstcur = self.dst.cursor();
-    if dbver!="raw":
-        self.dstcur.execute('drop table if exists abs_'+dbver+';');
-        self.dstcur.execute('drop table if exists dt_'+dbver+';');
-        self.dstcur.execute('drop table if exists td_'+dbver+';');
-        self.dstcur.execute('drop table if exists dic_'+dbver+';');
+    if self.table_name!="raw":
+        self.dstcur.execute('drop table if exists abs_'+self.table_name+';');
+        self.dstcur.execute('drop table if exists dt_'+self.table_name+';');
+        self.dstcur.execute('drop table if exists td_'+self.table_name+';');
+        self.dstcur.execute('drop table if exists dic_'+self.table_name+';');
     
-    self.dstcur.execute('create table abs_'+dbver+'(id int primary key, abs text);');
-    self.dstcur.execute('create table dic_'+dbver+'(id int primary key, term text);');
-    self.dstcur.execute('create table td_'+dbver+'(id int primary key, docs text);');
-    self.dstcur.execute('create table dt_'+dbver+'(id int primary key, terms text);');
+    self.dstcur.execute('create table abs_'+self.table_name+'(id int primary key, abs text);');
+    self.dstcur.execute('create table dic_'+self.table_name+'(id int primary key, term text);');
+    self.dstcur.execute('create table td_'+self.table_name+'(id int primary key, docs text);');
+    self.dstcur.execute('create table dt_'+self.table_name+'(id int primary key, terms text);');
 
   def __enter__(self):
     return self;
@@ -33,22 +30,22 @@ class dbConnector():
   
       
   def insertDoc(self,docID,abs, terms):
-    self.dstcur.execute('INSERT INTO abs_'+self.dbver+'(id, abs) VALUES (?, ?)', (docID,abs));
-    self.dstcur.execute('INSERT INTO dt_'+self.dbver+'(id, terms) VALUES (?, ?)', (docID,str(terms)));
+    self.dstcur.execute('INSERT INTO abs_'+self.table_name+'(id, abs) VALUES (?, ?)', (docID,abs));
+    self.dstcur.execute('INSERT INTO dt_'+self.table_name+'(id, terms) VALUES (?, ?)', (docID,str(terms)));
     for termID,num in terms.items():
-        self.dstcur.execute("select docs from td_"+self.dbver+" where id=?", (termID,))
+        self.dstcur.execute("select docs from td_"+self.table_name+" where id=?", (termID,))
         docsOfTerm= self.dstcur.fetchone()
         if docsOfTerm is None: # new term
             rec =str({docID:num})
-            self.dstcur.execute('INSERT INTO td_'+self.dbver+'(id, docs) VALUES (?, ?)', (termID,rec));
+            self.dstcur.execute('INSERT INTO td_'+self.table_name+'(id, docs) VALUES (?, ?)', (termID,rec));
         else:
             docsOfTerm=eval(docsOfTerm[0])
             docsOfTerm[docID]=num
             rec=str(docsOfTerm)
-            self.dstcur.execute("UPDATE td_"+self.dbver+" SET docs = ? WHERE id= ? """,(rec,termID))
+            self.dstcur.execute("UPDATE td_"+self.table_name+" SET docs = ? WHERE id= ? """,(rec,termID))
     self.dst.commit();
     
   def insertDic(self,dic):
       for i in range(len(dic)):
-          self.dstcur.execute('INSERT INTO dic_'+self.dbver+'(id, term) VALUES (?, ?)', (i,dic[i]));
+          self.dstcur.execute('INSERT INTO dic_'+self.table_name+'(id, term) VALUES (?, ?)', (i,dic[i]));
       self.dst.commit();
--- preprocessing/run.py
+++ preprocessing/run.py
@@ -3,23 +3,47 @@ import sys;
 import re;
 from preprocessing import DBUtil as lite
 from preprocessing import ExtractAbstract as ext
+import string
 
 def parse(doc):
+    """
+    Extracts abstract and ignores the tags extra white spaces within abstracts.
+    """
     doc=doc[doc.find('<abstract>'): doc.find('</abstract>')]
     doc= re.sub('<[^>]*>', '', doc[doc.find('<abstract>'): doc.find('</abstract>')])
-#     doc= re.sub('[\n\t&#]', '', doc)
     doc= re.sub('[\n\t]', '', doc)
     doc= re.sub('\s+', ' ', doc).encode('ascii', errors='backslashreplace').lower()
     return doc
 
+def get_stopwords():
+    Dic=[]
+    from nltk.corpus import stopwords
+    regex = re.compile('[%s]' % re.escape(string.punctuation))
+    with open('english.stop') as file:  # http://jmlr.csail.mit.edu/papers/volume5/lewis04a/a11-smart-stop-list/english.stop
+        jmlr_stopwords=file.readlines()
+        for word in jmlr_stopwords:
+            word=regex.sub(' ', word).split()
+            for w in word:
+                if w not in Dic:
+                    Dic.append(w)
+    Dic= list(set(Dic).union(set(stopwords.words('english'))))
+    return Dic
+    
 def clean(doc):
-    import string
-    doc=''.join([i for i in doc if not i.isdigit()])
-    exclude = set(string.punctuation)
-    doc = ''.join(ch for ch in doc if ch not in exclude)
+    regex = re.compile('[%s\d]' % re.escape(string.punctuation))
+    stopwords=get_stopwords()
+    doc=" ".join(w for w in doc.split() if not w in stopwords)
+    doc=regex.sub(' ', doc)
+    doc= re.sub('\s+', ' ', doc)
+    from stemming.porter2 import stem
+    doc = " ".join([stem(word) for word in doc.split(" ")])
+
     return doc
 
-def insertDic(dic,doc):
+def insertToDic(dic,doc):
+    """
+    Inserts new terms into dictionary and then returns the representation of the document
+    """
     words= doc.split()
     terms={}
     for w in words:
@@ -35,35 +59,50 @@ def insertDic(dic,doc):
             terms[idx] = 1
     return dic, terms
 
+def get_param(argv):
+    param={}
+    if len(argv)==1:
+        path='/home/arya/server/public/'
+        param['src']=path+'hctest.db'
+        param['dst']=path+'abstracts2.db'
+        param['pipeline']=['clean', 'parse']
+    else:
+        param['src']= argv[1]
+        param['dst']= argv[2]
+        param['pipeline']=[argv[3:]]
+    param['table_name'] = get_table_name(param['pipeline'])
+    return param
+
+def get_table_name(pipeline):
+    if 'clean' in pipeline:
+        return 'clean'
+    if 'parse' in pipeline:
+        return 'raw'
+        
+
 def main(argv):
     terms=[]
     dic=[]
     j=0
-    print len(argv)
-    if len(argv)==1:
-        path='/home/arya/abstracts.db'
-    else:
-        path= argv[1]
-    process='clean'
-    with lite.dbConnector(path,process) as db_conn:        
+    param=get_param(argv)
+    with lite.dbConnector(param) as db_conn:        
         while 1:
             j+=1
-            rec=db_conn.getROW()
+            rec=db_conn.getROW() # get a row from source database process it and insert it to destination databasr
             if rec is None:
                 break
-            print rec
-            if process == 'raw':
-                id,doc = rec[0], parse(rec[1])
-            else:
-                id,doc = rec[0], clean(rec[1])
-            print doc
-            dic, terms= insertDic(dic, doc)
+            id,doc = rec[0], rec[1]
+            if 'parse' in param['pipeline']:
+                id,doc = rec[0], parse(doc)
+            if 'clean' in param['pipeline']:
+                id,doc = rec[0], clean(doc)
+            dic, terms= insertToDic(dic, doc)
             if not id%100:
                 print id
             db_conn.insertDoc(id, doc ,terms)
-            if j>1:
+            if j>500:
                 break
         db_conn.insertDic(dic)
-    print 'Done'
+    print '\nDone!'
 if __name__ == '__main__':
     main(sys.argv);
