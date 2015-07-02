90283a8eb9b16f8277e60864dc3b41b55480ff35
arya <airanmehr@gmail.com>
2015-07-02 01:34:42 -0700

    run multiclass svm

--- ranking/rankMeSH.py
+++ ranking/rankMeSH.py
@@ -4,16 +4,20 @@ Created on Jun 26, 2015
 @author: arya
 '''
 import subprocess
-import os , pickle
+import os , pickle,sys
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import libsvm, LinearSVC,libsvm_sparse
+from sklearn.svm import libsvm, LinearSVC,libsvm_sparse,SVC
 from sklearn.datasets import load_svmlight_file
 import numpy as np
 from time import time
+import multiprocessing
 
+linear= not True
 
-
-def get_ranking(path='/home/arya/PubMed/GEO/Datasets/',split=0,linear=True,C=1,gamma=0.1):
+def get_ranking(split=0):
+    path='/home/arya/PubMed/GEO/Datasets/'
+    C=1
+    
     import warnings
     outpath='{}libsvm/out/'.format(path)
     if not os.path.exists(outpath):            os.makedirs(outpath)
@@ -21,12 +25,15 @@ def get_ranking(path='/home/arya/PubMed/GEO/Datasets/',split=0,linear=True,C=1,g
     start=time()
     warnings.filterwarnings('ignore')
     X, y = load_svmlight_file(dspath)
+    runname='.{}.{}'.format(('Nonlinear','Linear')[linear],split)
+    sys.stdout=open('{}model{}.log'.format(outpath,runname),'w')
+    sys.stderr=open('{}model{}.err'.format(outpath,runname),'w')
     if linear:
         model=OneVsRestClassifier(LinearSVC(random_state=0,C=C)).fit(X, y)
     else:
-        model=OneVsRestClassifier(libsvm(random_state=0,C=C, gamma=gamma)).fit(X, y)
-    pickle.dump(model,open('{}model_{}_C{}_G{}_{}.pkl'.format(outpath,('nonlinear','linear')[linear],C,gamma,split),'wb'))
-
+        gamma=0.1
+        model=OneVsRestClassifier(SVC(random_state=0,C=C, gamma=gamma)).fit(X, y)
+#     pickle.dump(model,open('{}model{}.pkl'.format(outpath,runname),'wb'))
     print 'learning...'
     X, y = load_svmlight_file(dspath.replace('.train','.test'))
     print 'predicting...'
@@ -35,10 +42,26 @@ def get_ranking(path='/home/arya/PubMed/GEO/Datasets/',split=0,linear=True,C=1,g
     labels=model.classes_
     ranking= labels[deci.argsort()[:,::-1]]
     print 'Accuracy:', np.mean(y==pred)
-    pickle.dump({'deci':deci,'y':y, 'pred':pred},open(outpath+'ranking.pkl','wb'))
+    pickle.dump({'deci':deci,'y':y, 'pred':pred},open('{}ranking{}.pkl'.format(outpath,runname),'wb'))
     print 'Done in {:.0f} minutes'.format((time()-start)/60.0)
     return ranking
     
 if __name__ == '__main__':
-    get_ranking()
-    
+    global linear
+    linear=True
+    numthreads=3
+    print 'Training', ('Nonlinear','Linear')[linear]
+    if numthreads==1:
+        for split in range(10):
+                get_ranking(split)
+    else:
+        pool=multiprocessing.Pool(numthreads)
+        pool.map(get_ranking,range(10))
+    linear=False
+    if numthreads==1:
+        for split in range(10):
+                get_ranking(split)
+    else:
+        pool=multiprocessing.Pool(numthreads)
+        pool.map(get_ranking,range(10))
+    print 'Done'
